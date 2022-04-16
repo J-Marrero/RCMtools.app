d37a413483910c54dd7b382ed5b257c0493ea2b7
Josh <personal.marrero@gmail.com>
2022-04-16 02:09:01 -0400

    Ensure that Duplicates are supressed Fixes #2 # Changes Made - Added better documentation - Combined CPT Checking and DX checking into a single function - Deleted the old functions for CPT and DX parsing - Added responsibility to RowFactory
    Complete!

--- NCDToolScripts.js
+++ NCDToolScripts.js
@@ -54,17 +54,17 @@ function clearTable(className){
    }
 };
 
-                                                                                                   //factory to create rows from arrays of raw data provided by the 'get_' functions listed next
+                                                                                                   //factory to create rows from arrays of raw data provided by the check function (YOUR ONE STOP SHOP FOR ALL YOUR CPT AND DX NEEDS)
                                                                                                    //requires the use of "del_plcholder" to indicate a fancy delete button
-function rowFactory(content){
+function rowFactory(content, responsible){
     if(Array.isArray(content) == false){
       throw Error("The content provided is not an Array!")
     }
     if(content.length != 4||5){
       if(content.length < 4){
-        throw Error("The array provided is too short!")
+        throw Error("The array provided is too short!\n"+responsible)
       } else if(content.length > 5){
-        throw Error("The array provided is too long!")
+        throw Error("The array provided is too long!\n"+responsible)
       }
     }
     var rowTop = document.createElement("tr")
@@ -83,82 +83,29 @@ function rowFactory(content){
     return rowTop
 };
 
-// function get_DxCodes(x) {
-//     let dx_target = document.getElementsByClassName("DX_body")[0]
-//     let regexp = /[A-TV-Z][0-9][0-9AB]\.?[0-9A-TV-Z]{0,4}/g;
-//     let matchAll = x.matchAll(regexp);
-//     matchAll = Array.from(matchAll);
-//     for(var v of matchAll){
-//         if(duplicateCode(v) != true){
-//         var resp_array = []
-//         fetch("http://icd10api.com/?code="+ v + "&desc=short&r=json")
-//         .then(response => response.json())
-//         .then(data => {
-//           if(data.Valid == 1){
-//               resp_array.push(data.Name.substring(0, 3) + "." + data.Name.substring(3, x.length))
-//               resp_array.push(data.Description)
-//           if(NCS.Contents.includes(v)){
-//               resp_array.push("🔴")
-//               } else {
-//               resp_array.push("🟢")
-//               }
-//               resp_array.push("del_plcholder")
-//               dx_target.appendChild(rowFactory(resp_array))
-//               resp_array = []          
-
-//           }
-//           if(data.Valid != 1){
-//               resp_array.push(v)
-//               resp_array.push(data.Error)
-//               resp_array.push("🚩")
-//               resp_array.push("del_plcholder")
-//               dx_target.appendChild(rowFactory(resp_array))
-//               resp_array = []
-//           }
-//         })
-
-//         } else {throw Error(v + " is a Duplicate Diagnosis code.")}
-//     } 
-// };
-
-// function get_CPTCodes(x) {
-//     let cpt_target = document.getElementsByClassName("cpt_body")[0]
-//     let regexp = /\d{4,4}[A-Z0-9]/g;
-//     let matchAll = x.matchAll(regexp);
-//     matchAll = Array.from(matchAll);
-//     for(var v of matchAll){
-//         var resp_Array = []
-//         resp_Array.push(v[0])
-//         for(var NCD_Instance of Index){
-//             if(NCD_Instance.CPT_Contents.includes(v[0])){
-//                 resp_Array.push(NCD_Instance.Title)
-//             }
-//         }
-//         for(var Descrip_Instance of CPT_Descrip){
-//             if(Descrip_Instance.CPT == v[0]){
-//              resp_Array.push(Descrip_Instance.Description)
-//             }
-//         }
-//         resp_Array.push("")
-//         resp_Array.push("del_plcholder")
-//         cpt_target.appendChild(rowFactory(resp_Array))
-//     }
-// };
-
-// //helperfunction until Dx and Cpt checking can be integrated into a single function
-
-// function check(x){
-// get_CPTCodes(x)
-// get_DxCodes(x)
-// }
-
-// function duplicateCode(dxCode){
-//     //determine if this is a duplicate code
-//     var Diagnosis_Element_Array = Array.from(document.getElementsByClassName("DX_body")[0].rows)
-//     var Existing_Codes = []
-//     Diagnosis_Element_Array.forEach(element => Existing_Codes.push(element.childNodes[0].innerText));
-//     return Existing_Codes.includes(dxCode)
-// }
+function get_CPTCodes(x) {
+    let cpt_target = document.getElementsByClassName("cpt_body")[0]
+    let regexp = /\d{4,4}[A-Z0-9]/g;
+    let matchAll = x.matchAll(regexp);
+    matchAll = Array.from(matchAll);
+    for(var v of matchAll){
+        var resp_Array = []
+        resp_Array.push(v[0])
+        for(var NCD_Instance of Index){
+            if(NCD_Instance.CPT_Contents.includes(v[0])){
+                resp_Array.push(NCD_Instance.Title)
+            }
+        }
+        for(var Descrip_Instance of CPT_Descrip){
+            if(Descrip_Instance.CPT == v[0]){
+             resp_Array.push(Descrip_Instance.Description)
+            }
+        }
+        resp_Array.push("")
+        resp_Array.push("del_plcholder")
+        cpt_target.appendChild(rowFactory(resp_Array))
+    }
+};
 
                                                                                                    //creates Random Test data
 function throwRandom(){
@@ -189,24 +136,79 @@ function KeyPress(e) {
 
 document.onkeydown = KeyPress;
 
-function check(){
-    var userInput = document.getElementsByTagName("textarea")[0].value
-    if(userInput == null){
+function check() {
+    let cpt_target = document.getElementsByClassName("cpt_body")[0]                                //locate targets for the following function's DOM output
+    let dx_target = document.getElementsByClassName("DX_body")[0]
+
+    var userInput = document.getElementsByTagName("textarea")[0].value                             //Get the user's input
+    if (userInput == null) {                                                                       //Handle a lack of input (Do user stuff here when no input is present)
         Error("No user input to parse")
     }
 
-    let dxregexp = /[A-TV-Z][0-9][0-9AB]\.?[0-9A-TV-Z]{0,4}/g;                                     //do the Dx Codes first
+    var ParsedDx = []                                                                              //Run RegEx to find and get Diangnosis codes and put them into an array called ParsedDx
+    let dxregexp = /[A-TV-Z][0-9][0-9AB]\.?[0-9A-TV-Z]{0,4}/g;                                     
     let matchAlldx = userInput.matchAll(dxregexp)
-    matchAlldx = Array.from(matchAlldx);
+    matchAlldx = Array.from(matchAlldx)
+    matchAlldx.forEach(element => { ParsedDx.push(element[0]) });
 
-    let cptregexp = /\d{4,4}[A-Z0-9]/g
+    var ParsedCPT = []                                                                             //Run RegEx to find and get CPT codes and put them into an array called ParsedCPT
+    let cptregexp = /\d{4,4}[A-Z0-9]/g;
     let matchAllcpt = userInput.matchAll(cptregexp)
-    matchAllcpt = Array.from(matchAlldx);
-    
-    console.log(matchAlldx)
-    console.log(matchAllcpt)
+    matchAllcpt = Array.from(matchAllcpt)
+    matchAllcpt.forEach(element => { ParsedCPT.push(element[0]) });
 
-    if(checkSetting("NCD_Settings","Supress_Duplicates") == true){                                 //checks to see if duplicate supression is on
-      //console.log(userInput.split('\n'))      
+    if (checkSetting("NCD_Settings", "Supress_Duplicates") == true) {                              //checks localstorage settings ("Settings.NCD_Settings.Supress_Duplicates") to see if duplicate supression is on
+        var CPT = Array.from(new Set(ParsedCPT))                                                   //if Duplicate supression is on, process the arrays made from regEx into sets into arrays (we want the elements to come out of this process unified and the else part of this statement will just rename the array from the prior step)
+        var DX = Array.from(new Set(ParsedDx))
+    } else {
+        var CPT = ParsedCPT
+        var DX = ParsedDx
     }
+    
+    for (var v of DX) {                                                                            //for each diagnosis from the prior step (duplicate-safe or duplicate-dangerous)
+        var resp_array = []
+        fetch("http://icd10api.com/?code=" + v + "&desc=short&r=json")                             //send them to a free dx API found on the internet
+            .then(response => response.json())                                                     //recieve responses
+            .then(data => {
+                if (data.Valid == 1) {                                                             //if the API response indicates that the user passed diagnosis code is valid
+                    resp_array.push(data.Name.substring(0, 3) + "." + data.Name.substring(3, v.length))      //do a bunch of checking in the ncd tables
+                    resp_array.push(data.Description)
+                    if (NCS.Contents.includes(v)) {
+                        resp_array.push("🔴")
+                    } else {
+                        resp_array.push("🟢")
+                    }
+                    resp_array.push("del_plcholder")
+                    dx_target.appendChild(rowFactory(resp_array,"DX_PARSE"))
+                    resp_array = []
+
+                }
+                if (data.Valid != 1) {
+                    resp_array.push(v)
+                    resp_array.push(data.Error)
+                    resp_array.push("🚩")
+                    resp_array.push("del_plcholder")
+                    dx_target.appendChild(rowFactory(resp_array,"DX_PARSE"))
+                    resp_array = []
+                }
+            })
+    };
+    for(var v of CPT){
+        var resp_Array = []
+        resp_Array.push(v)
+        for(var NCD_Instance of Index){
+            if(NCD_Instance.CPT_Contents.includes(v)){
+                resp_Array.push(NCD_Instance.Title)
+            }
+        }
+        for(var Descrip_Instance of CPT_Descrip){
+            if(Descrip_Instance.CPT == v){
+             resp_Array.push(Descrip_Instance.Description)
+            }
+        }
+        resp_Array.push("")
+        resp_Array.push("del_plcholder")
+        cpt_target.appendChild(rowFactory(resp_Array,"CPT_PARSE"))
+    }
+
 }
