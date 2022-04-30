4e983bc8786316a8fdcdef63e8aef19453032f9f
Josh <personal.marrero@gmail.com>
2022-04-29 23:09:56 -0400

    Added a possible additional Feature ("Relational Diagrams") more to come soon

---
+++ RelationalDiagram.js
@@ -0,0 +1,98 @@
+var line1=	["Queue","Invoice_Number","Tx","Type","Description","Service_Date","Status","Amount","Insurance","Self_Pay","Outstanding","Patient","Department","Bill_Area ","Responsible_Payer","Primary_Dx","ETR_ID","TX_From","TxTo","Visit"]
+var line2=	["","PHNY1005355440","5","Charge","U0003-PR COV-19 AMP PRB HGH THRUPUT","44463","","300","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 1","Z00.00","5","","","1"]
+var line3=	["","PHNY1005355440","4","Charge","U0005-PR INFEC AGEN DETEC AMPLI PROBE","44463","","75","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 1","Z00.00","4","","","1"]
+var line4=	["","PHNY1005355440","3","Charge","87426 (CPT®)-PR INFECT AGENT ANTIGEN DETECT BY IMMUNOASSAY TECH, ELISA, IMCA QUAL OR QUANT, MULTI-STEP; SEVERE ACUTE RESP SYNDROME CORONAVIRUS","44463","","150","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 1","Z00.00","3","","","1"]
+var line5=	["","PHNY1005355440","2","Charge","99000 (CPT®)-PR HANDLG&/OR CONVEY OF SPEC FOR TR OFFICE TO LAB","44463","","20","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 1","Z00.00","2","","","1"]
+var line6=	["","PHNY1005355440","1","Charge","99213 (CPT®)-PR OFFICE OUTPATIENT VISIT 15 MINUTES","44463","","260","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 1","Z00.00","1","","","1"]
+var line7=	["","PHNY1005355441","5","Charge","U0003-PR COV-19 AMP PRB HGH THRUPUT","44463","","300","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 1","Z00.00","5","","","1"]
+var line8=	["","PHNY1005355441","4","Charge","U0005-PR INFEC AGEN DETEC AMPLI PROBE","44463","","75","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 1","Z00.00","4","","","1"]
+var line9=	["","PHNY1005355441","3","Charge","87426 (CPT®)-PR INFECT AGENT ANTIGEN DETECT BY IMMUNOASSAY TECH, ELISA, IMCA QUAL OR QUANT, MULTI-STEP; SEVERE ACUTE RESP SYNDROME CORONAVIRUS","44463","","150","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 1","Z00.00","3","","","1"]
+var line10=	["","PHNY1005355441","2","Charge","99000 (CPT®)-PR HANDLG&/OR CONVEY OF SPEC FOR TR OFFICE TO LAB","44463","","20","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 1","Z00.00","2","","","1"]
+var line11=	["","PHNY1005355441","1","Charge","99213 (CPT®)-PR OFFICE OUTPATIENT VISIT 15 MINUTES","44463","","260","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 1","Z00.00","1","","","1"]
+var line12=	["","PHNY1006862000","20","Charge","90461 (CPT®)-PR IMMUNIZ ADMIN, THRU AGE 18, ANY ROUTE,W COUNSEL, EA ADD VACCINE/TOXOID","44476","","45","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 2","Z00.00","964409","","","2"]
+var line13=	["","PHNY1006862000","19","Charge","90461 (CPT®)-PR IMMUNIZ ADMIN, THRU AGE 18, ANY ROUTE,W COUNSEL, EA ADD VACCINE/TOXOID","44476","","45","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 2","Z00.00","964407","","","2"]
+var line14=	["","PHNY1006862000","18","Charge","90460 (CPT®)-PR IMMUNIZ ADMIN, THRU AGE 18, ANY ROUTE,W COUNSEL, 1ST VACCINE/TOXOID","44476","","60","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 2","Z00.00","964406","","","2"]
+var line15=	["","PHNY1006862000","17","Charge","90460 (CPT®)-PR IMMUNIZ ADMIN, THRU AGE 18, ANY ROUTE,W COUNSEL, 1ST VACCINE/TOXOID","44476","","60","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 2","Z00.00","964404","","","2"]
+var line16=	["","PHNY1006862000","16","Charge","99173 (CPT®)-PR VISUAL SCREENING TEST, BILAT","44476","","15","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 2","Z00.00","964402","","","2"]
+var line17=	["","PHNY1006862000","15","Charge","92552 (CPT®)-PR PURE TONE AUDIOMETRY, AIR","44476","","115","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 2","Z00.00","964401","","","2"]
+var line18=	["","PHNY1006862000","14","Charge","96127 (CPT®)-PR BEHAV ASSMT W/SCORE & DOCD/STAND INSTRUMENT","44476","","20","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 2","Z00.00","964399","","","2"]
+var line19=	["","PHNY1006862000","13","Charge","99393 (CPT®)-PR PREVENTIVE VISIT,EST,AGE5-11","44476","","450","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 2","Z00.00","964398","","","2"]
+var line20=	["","PHNY1006862000","12","Charge","81003 (CPT®)-PR URINALYSIS, AUTO, W/O SCOPE","44476","","5","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 2","Z00.00","964397","","","2"]
+var line21=	["","PHNY1006862000","11","Charge","90715 (CPT®)-PR TDAP VACCINE >7 YO, IM","44476","","105","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 2","Z00.00","964396","","","2"]
+var line22=	["","PHNY1006862000","10","Charge","90686 (CPT®)-PR IIV4 VACC PRESRV FREE 3 YRS & OLDER IM USE","44476","","60","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 2","Z00.00","964395","","","2"]
+var line23=	["","PHNY1006862001","20","Charge","90461 (CPT®)-PR IMMUNIZ ADMIN, THRU AGE 18, ANY ROUTE,W COUNSEL, EA ADD VACCINE/TOXOID","44476","","45","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 2","Z00.00","964409","","","2"]
+var line24=	["","PHNY1006862001","19","Charge","90461 (CPT®)-PR IMMUNIZ ADMIN, THRU AGE 18, ANY ROUTE,W COUNSEL, EA ADD VACCINE/TOXOID","44476","","45","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 2","Z00.00","964407","","","2"]
+var line25=	["","PHNY1006862001","18","Charge","90460 (CPT®)-PR IMMUNIZ ADMIN, THRU AGE 18, ANY ROUTE,W COUNSEL, 1ST VACCINE/TOXOID","44476","","60","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 2","Z00.00","964406","","","2"]
+var line26=	["","PHNY1006862001","17","Charge","90460 (CPT®)-PR IMMUNIZ ADMIN, THRU AGE 18, ANY ROUTE,W COUNSEL, 1ST VACCINE/TOXOID","44476","","60","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 2","Z00.00","964404","","","2"]
+var line27=	["","PHNY1006862001","16","Charge","99173 (CPT®)-PR VISUAL SCREENING TEST, BILAT","44476","","15","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 2","Z00.00","964402","","","2"]
+var line28=	["","PHNY1006862001","15","Charge","92552 (CPT®)-PR PURE TONE AUDIOMETRY, AIR","44476","","115","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 2","Z00.00","964401","","","2"]
+var line29=	["","PHNY1006862001","14","Charge","96127 (CPT®)-PR BEHAV ASSMT W/SCORE & DOCD/STAND INSTRUMENT","44476","","20","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 2","Z00.00","964399","","","2"]
+var line30=	["","PHNY1006862001","13","Charge","99393 (CPT®)-PR PREVENTIVE VISIT,EST,AGE5-11","44476","","450","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 2","Z00.00","964398","","","2"]
+var line31=	["","PHNY1006862001","12","Charge","81003 (CPT®)-PR URINALYSIS, AUTO, W/O SCOPE","44476","","5","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 2","Z00.00","964397","","","2"]
+var line32=	["","PHNY1006862001","11","Charge","90715 (CPT®)-PR TDAP VACCINE >7 YO, IM","44476","","105","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 2","Z00.00","964396","","","2"]
+var line33=	["","PHNY1006862001","10","Charge","90686 (CPT®)-PR IIV4 VACC PRESRV FREE 3 YRS & OLDER IM USE","44476","","60","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 2","Z00.00","964395","","","2"]
+var line34=	["","PHNY1006862002","20","Charge","90461 (CPT®)-PR IMMUNIZ ADMIN, THRU AGE 18, ANY ROUTE,W COUNSEL, EA ADD VACCINE/TOXOID","44476","","45","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 2","Z00.00","964409","","","2"]
+var line35=	["","PHNY1006862002","19","Charge","90461 (CPT®)-PR IMMUNIZ ADMIN, THRU AGE 18, ANY ROUTE,W COUNSEL, EA ADD VACCINE/TOXOID","44476","","45","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 2","Z00.00","964407","","","2"]
+var line36=	["","PHNY1006862002","18","Charge","90460 (CPT®)-PR IMMUNIZ ADMIN, THRU AGE 18, ANY ROUTE,W COUNSEL, 1ST VACCINE/TOXOID","44476","","60","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 2","Z00.00","964406","","","2"]
+var line37=	["","PHNY1006862002","17","Charge","90460 (CPT®)-PR IMMUNIZ ADMIN, THRU AGE 18, ANY ROUTE,W COUNSEL, 1ST VACCINE/TOXOID","44476","","60","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 2","Z00.00","964404","","","2"]
+var line38=	["","PHNY1006862002","16","Charge","99173 (CPT®)-PR VISUAL SCREENING TEST, BILAT","44476","","15","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 2","Z00.00","964402","","","2"]
+var line39=	["","PHNY1006862002","15","Charge","92552 (CPT®)-PR PURE TONE AUDIOMETRY, AIR","44476","","115","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 2","Z00.00","964401","","","2"]
+var line40=	["","PHNY1006862002","14","Charge","96127 (CPT®)-PR BEHAV ASSMT W/SCORE & DOCD/STAND INSTRUMENT","44476","","20","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 2","Z00.00","964399","","","2"]
+var line41=	["","PHNY1006862002","13","Charge","99393 (CPT®)-PR PREVENTIVE VISIT,EST,AGE5-11","44476","","450","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 2","Z00.00","964398","","","2"]
+var line42=	["","PHNY1006862002","12","Charge","81003 (CPT®)-PR URINALYSIS, AUTO, W/O SCOPE","44476","","5","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 2","Z00.00","964397","","","2"]
+var line43=	["","PHNY1006862002","11","Charge","90715 (CPT®)-PR TDAP VACCINE >7 YO, IM","44476","","105","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 2","Z00.00","964396","","","2"]
+var line44=	["","PHNY1006862002","10","Charge","90686 (CPT®)-PR IIV4 VACC PRESRV FREE 3 YRS & OLDER IM USE","44476","","60","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 2","Z00.00","964395","","","2"]
+var line45=	["","PHNY1006693170","7","Charge","36415 (CPT®)-CHG COLLECTION VENOUS BLOOD,VENIPUNCTURE","44480","Retro","10","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 2","Z00.00","922633","","48","3"]
+var line46=	["","PHNY1006693180","9","Charge","99000 (CPT®)-PR HANDLG&/OR CONVEY OF SPEC FOR TR OFFICE TO LAB","44480","Retro","20","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 2","Z00.00","922641","","51","3"]
+var line47=	["","PHNY1006693180","8","Charge","36415 (CPT®)-CHG COLLECTION VENOUS BLOOD,VENIPUNCTURE","44480","Retro","10","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 2","Z00.00","922640","","50","3"]
+var line48=	["","PHNY1006813110","23","Charge","80061 (CPT®)-PR LIPID PANEL","44480","Retro","45","0","0","0","Foosball, Nut","Department B","Billing Place of Service","","Z00.00","997806","","56","3"]
+var line49=	["","PHNY1006813110","22","Charge","80053 (CPT®)-PR METABOLIC PANEL,COMPREHENSIVE","44480","Retro","35","0","0","0","Foosball, Nut","Department B","Billing Place of Service","","Z00.00","997805","","55","3"]
+var line50=	["","PHNY1006813110","21","Charge","85025 (CPT®)-PR COMPLETE CBC & AUTO DIFF WBC","44480","Retro","25","0","0","0","Foosball, Nut","Department B","Billing Place of Service","","Z00.00","997804","","54","3"]
+var line51=	["W","PHNY1024867300","56","Charge","80061 (CPT®)-PR LIPID PANEL","44480","","45","45","0","45","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 1","Z00.00","12585325","23","","3"]
+var line52=	["W","PHNY1024867300","55","Charge","80053 (CPT®)-PR METABOLIC PANEL,COMPREHENSIVE","44480","","35","35","0","35","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 1","Z00.00","12585324","22","","3"]
+var line53=	["W","PHNY1024867300","54","Charge","85025 (CPT®)-PR COMPLETE CBC & AUTO DIFF WBC","44480","","25","25","0","25","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 1","Z00.00","12585323","21","","3"]
+var line54=	["","PHNY1024867310","48","Charge","36415 (CPT®)-CHG COLLECTION VENOUS BLOOD,VENIPUNCTURE","44480","","10","10","0","10","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 1","Z00.00","12550494","7","","3"]
+var line55=	["","PHNY1024867320","51","Charge","99000 (CPT®)-PR HANDLG&/OR CONVEY OF SPEC FOR TR OFFICE TO LAB","44480","Susp NRP","10","0","0","0","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 1","Z00.00","12550498","9","","3"]
+var line56=	["","PHNY1024867320","50","Charge","36415 (CPT®)-CHG COLLECTION VENOUS BLOOD,VENIPUNCTURE","44480","","10","10","0","10","Foosball, Nut","Department B","Billing Place of Service","Evil Insurance Company 1","Z00.00","12550497","8","","3"]
+
+var dataset = [line2, line3, line4, line5, line6, line7, line8, line9, line10, line11, line12, line13, line14, line15, line16, line17, line18, line19, line20, line21, line22, line23, line24, line25, line26, line27, line28, line29, line30, line31, line32, line33, line34, line35, line36, line37, line38, line39, line40, line41, line42, line43, line44, line45, line46, line47, line48, line49, line50, line51, line52, line53, line54, line55, line56]
+var Header = line1
+
+var objectSchema = {invoiceLevelInfo : ['Invoice_Number', 'Service_Date', 'Responsible_Payer', 'Transactions'],
+                    visitLevelInfo : ['Visit', 'Patient', 'Invoices'],
+                    transactionLevelInfo : ['Amount', 'Insurance', 'Self_Pay', 'Outstanding', 'Queue', 'Status', 'Bill_Area', 'Department', 'Description', 'Primary_Dx', 'Type', 'TX_From', 'Tx', 'Tx_To', 'ETR_ID']}
+
+
+//function to create objects for each line
+
+function address(keys,values){
+    var createdObject = new Object()
+    for(var key of keys){
+        var value = values[keys.indexOf(key)]
+        if(value == ""){
+            createdObject[key] = null
+        } else {
+            createdObject[key] = value
+        }
+    }
+    return createdObject
+}
+
+//routine that turns the body array and lines into a 'bucket' of objects
+
+var objectPlace = []
+for(line of dataset){
+    objectPlace.push(address(Header, line))
+}
+
+//For paring down objectPlace
+
+function getUnique(dataset,property){
+    var F = []
+    for(var V of dataset){
+        F.push(V[property])
+    }
+    var F = new Set(F)
+        return F
+}
+
