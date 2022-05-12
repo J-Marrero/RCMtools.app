720a541cdfd8bd2519ac3088cbdd945c9863fe1e
J-Marrero <personal.marrero@gmail.com>
2022-05-12 00:30:41 -0400

    Additional changes to the retrieval script, will go to medicare site and retrieve the file itself. Simplifies the user input to binary.

--- PDF Rule Composer/Composer/composer.py
+++ PDF Rule Composer/Composer/composer.py
@@ -1,23 +1,30 @@
-# requires use of "pyexcel-xlsxr", "pyexcel",
-
+# requires use of "pyexcel-xlsxr", "pyexcel", "BeautifulSoup4"
+from bs4 import BeautifulSoup
+import BeautifulSoup as soup
 import pyexcel as excel
 import zipfile
 import os
+import urllib.request
 
-# declare the locations of where the file comes from.
-
-web_file_location = "https://www.cms.gov/Medicare/Coverage/CoverageGenInfo/LabNCDsICD10"
-local_file_location = ".\PDF Rule Composer\Composer\april-2022-lab-code-list-icd-10.zip"
+# procure the current working directory
 current_working_dir = os.system("cd")
+
+#get Page with files on it
+web_file_location = "https://www.cms.gov/Medicare/Coverage/CoverageGenInfo/LabNCDsICD10"
+os.system("wget "+web_file_location)
+
+#get the page that contains the links to excel files of NCD Policies. This is also where changes will be detected.
+mcar_file = urllib.request.urlopen(web_file_location)
+
 # Assuming that we have previously retrieved the required file from the NCD webpage we will load it "NCD.xlsx"
 
 # Use MyZip to unzip the retrieved archive
-myzip = zipfile.ZipFile(local_file_location,'r')
+#myzip = zipfile.ZipFile(local_file_location,'r')
 # this creates a folder in the incorrect place, needs to be in the same directory as the file being run.
-myzip.extractall(current_working_dir)
-myzip.close()
+#myzip.extractall(current_working_dir)
+#myzip.close()
 
 
-sheets = ()
+#sheets = ()
 
-sheet = p.get_sheet(file_name="Temporary Unzipped File\2022200-Initial-ICD10-NCD-Spreadsheet-20211129.xlsx")
+#sheet = p.get_sheet(file_name="Temporary Unzipped File\2022200-Initial-ICD10-NCD-Spreadsheet-20211129.xlsx")
